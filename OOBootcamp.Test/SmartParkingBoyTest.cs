6af23e929d1d115b894342bae6693a71bf31c11e
Shiyang Liu <shiyang.liu@thoughtworks.com>
2022-09-11 00:23:29 +0800

    write a test for B has more avaiable count

--- OOBootcamp.Test/SmartParkingBoyTest.cs
+++ OOBootcamp.Test/SmartParkingBoyTest.cs
@@ -35,5 +35,21 @@ public class SmartParkingBoyTest
         Assert.That(parkingLots[1].AvailableCount, Is.EqualTo(10));
         Assert.That(parkingLots[2].AvailableCount, Is.EqualTo(10));
     }
+    
+    // Task1: Given 3 parkinglots and B has most available count When park a car Then park in B
+    [Test]
+    public void should_park_in_B_parkinglot_when_B_has_most_avaliablecount()
+    {
+        parkingLots[0].AvailableCount = 9;
+        parkingLots[1].AvailableCount = 10;
+        parkingLots[2].AvailableCount = 9;
+
+        var boy = new SmartParkingBoy(parkingLots);
+        boy.Park(car);
+        
+        Assert.That(parkingLots[0].AvailableCount, Is.EqualTo(9));
+        Assert.That(parkingLots[1].AvailableCount, Is.EqualTo(9));
+        Assert.That(parkingLots[2].AvailableCount, Is.EqualTo(9));
+    }
 
 }
