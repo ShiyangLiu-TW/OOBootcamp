d5faf275adce254bf4b2aa81dd40ba883cb1ff65
Shiyang Liu <shiyang.liu@thoughtworks.com>
2022-09-10 21:30:36 +0800

    inplement class SmartParkingBoy

--- OOBootcamp/SmartParkingBoy.cs
+++ OOBootcamp/SmartParkingBoy.cs
@@ -2,5 +2,16 @@ namespace OOBootcamp;
 
 public class SmartParkingBoy
 {
-    
+    private List<ParkingLot> _parkingLots;
+    public SmartParkingBoy(List<ParkingLot> parkingLots)
+    {
+        this._parkingLots = parkingLots;
+    }
+
+    public void Park(Vehicle car)
+    {
+        var nextParkingLot = _parkingLots.Where(p => p.AvailableCount != 0)
+            .OrderByDescending(p => p.AvailableCount / p.MaxCapacity).First();
+        nextParkingLot.ParkVehicle(car);
+    }
 }
